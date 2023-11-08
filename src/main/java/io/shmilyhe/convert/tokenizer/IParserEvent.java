bdc834fc54e0ad385c69bd1050d18329248421af
shmilyhe <shmilyhe@163.com>
2023-11-08 19:55:27 +0800

    Ast

--- src/main/java/io/shmilyhe/convert/tokenizer/IParserEvent.java
+++
@@ -1,9 +0,0 @@
-package io.shmilyhe.convert.tokenizer;
-
-public interface IParserEvent {
-   void nextLine();
-   void text(String str,int line,int col);
-   void symbol(String str,int line,int col);
-   void function(String str,int line,int col);
-   Block getBlock();
-}
